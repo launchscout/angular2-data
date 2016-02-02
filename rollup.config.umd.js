8f37458934a9d5258dd6eb270085f6d5956281aa
Chris Nelson <superchrisnelson@gmail.com>
2016-02-02 12:00:58 -0500

    module naming

--- rollup.config.umd.js
+++ rollup.config.umd.js
@@ -2,7 +2,7 @@ import config from './rollup.config';
 import typescript from 'rollup-plugin-typescript';
 
 config.format = 'umd';
-config.dest = 'dist/rollup-starter-project.umd.js';
-config.moduleName = 'rollupStarterProject';
+config.dest = 'dist/angular2-data.umd.js';
+config.moduleName = 'angular2data';
 
 export default config;
